e5198e1850d37e44dca880c5ebda273fda2f3934
Kajiura Teruno <kajiteru1029@gmail.com>
2022-07-11 11:57:57 +0900

    入出力形式の修正

--- mpt5/mask40.py
+++ mpt5/mask40.py
@@ -5,10 +5,6 @@ import csv
 import pandas as pd
 import re
 
-# 事前学習用テキストデータから読み出す --> SRC, TGTのペアになるtsvファイルを出力
-readfile = "/content/conala-mini.txt" 
-writefile = "random_mask40.tsv"
-
 def get_token(text): #conalaのBleuからtokenizerを拝借
     text = re.sub(r'([^A-Za-z0-9_])', r' \1 ', text)
     text = re.sub(r'([a-z])([A-Z])', r'\1 \2', text)
@@ -18,38 +14,38 @@ def get_token(text): #conalaのBleuからtokenizerを拝借
     tokens = [t for t in text.split(' ') if t]
     return tokens
 
-def mask(typ_, typ_idx):
-  typ_mask = []
-  for index in typ_idx:
-    if random.random() < 0.40:       # Mask rate
+def mask(s:str, ratio):
+  token = get_token(s)
+  token_idx = [idx for idx in range(len(token))] 
+  buffer = []
+  
+  for index in token_idx:
+    if random.random() < ratio:       # Mask rate
       if random.random() < 0.8:     # 40% of the time, replace with [MASK]
         masked_token = "[MASK]"
         if index == 0:
-          typ_mask.append(masked_token)
+          buffer.append(masked_token)
         else:
-          if masked_token != typ_mask[-1]: #ひとつ前がMASKではなかったらMASKを追加
-            typ_mask.append(masked_token)
+          if masked_token != buffer[-1]: #ひとつ前がMASKではなかったらMASKを追加
+            buffer.append(masked_token)
           else:
             pass
       elif random.random() > 0.8 and random.random() < 0.9: # 10% of the time, replace with random word
-        masked_token = typ_[random.randint(0, len(typ_idx) - 1)]
-        typ_mask.append(masked_token)
+        masked_token = token[random.randint(0, len(token_idx) - 1)]
+        buffer.append(masked_token)
       else:      # 10% of the time, keep original
-        typ_mask.append(typ_[index])
+        buffer.append(token[index])
     else:
-      typ_mask.append(typ_[index])
-  typ_mask = "".join(typ_mask)
-  return typ_mask
+     buffer.append(token[index])
+  buffer = "".join(buffer)
+  return buffer
 
-def main():
-  with open(readfile) as f: #読み込み用ファイル
-    with open(writefile, 'w') as f2:
-      writer = csv.writer(f2, delimiter='\t')
-      for line in f:
-        token_ = get_token(line)
-        token_idx = [idx for idx in range(len(token_))] 
-        token_mask = mask(token_, token_idx)
-        
-        src_tgt = [token_mask, line]
-        print(src_tgt)
-        writer.writerow(src_tgt)
+# 事前学習用テキストデータから読み出す
+readfile = "/content/conala-mined.txt"
+writefile = "/content/random_mask40.txt"
+
+with open(readfile) as f: #読み込み用ファイル
+  with open(writefile, 'w') as f2:
+    for line in f:
+      token_mask = mask(line, 0.4)
+      f2.write(token_mask + '\n')
