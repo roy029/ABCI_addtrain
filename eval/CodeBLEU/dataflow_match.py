686aa047f6f21b31f9e9256231137aa1664ebe77
kajiura <79959901+roy029@users.noreply.github.com>
2023-01-20 20:36:08 +0900

    Update dataflow_match.py

--- eval/CodeBLEU/dataflow_match.py
+++ eval/CodeBLEU/dataflow_match.py
@@ -1,7 +1,8 @@
 # Copyright (c) Microsoft Corporation. 
 # Licensed under the MIT license.
 
-from parser import DFG_python, DFG_java, DFG_ruby, DFG_go, DFG_php, DFG_javascript, DFG_csharp
+from parser import DFG_python
+# from parser import DFG_python, DFG_java, DFG_ruby, DFG_go, DFG_php, DFG_javascript, DFG_csharp
 from parser import (remove_comments_and_docstrings,
                                        tree_to_token_index,
                                        index_to_code_token,
